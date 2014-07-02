3808148b17ff935671cc0d0e86c889bad6fed1a9
Zach Latta <zach@hackedu.us>
2014-07-01 20:40:14 -0700

    Trailing slash in yoall call.

--- yoall.go
+++ yoall.go
@@ -8,7 +8,7 @@ import (
 // YoAll sends a Yo to all of your subscribers. The API token is issued by Yo
 // for a specific account and is required.
 func (c *Client) YoAll(apiToken string) (*http.Response, error) {
-	u := "yoall"
+	u := "yoall/"
 	body := url.Values{"api_token": {apiToken}}
 	req, err := c.NewRequestForm(u, body)
 	if err != nil {
